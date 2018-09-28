9f0da3b12b2ebe9758435bdec679874a13ebb8c7
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2018-09-28 13:40:46 -0400

    Created dummy data and hooked up server and controller and pass data to frontend

--- public/controllers/customerController.js
+++ public/controllers/customerController.js
@@ -2,4 +2,9 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('appCtrl', ['$scope', '$http',	function($scope, $http) {
 	console.log("Hello world from the controller");
+
+	$http.get('/customerList').then(function(response) {
+		console.log("I got the data I requested");
+		$scope.customerList = response.data;
+	});
 }]);
--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
 
-// app.get('/', function(request, response) {
-//     response.send("Hello world from server.js");
-// });
 app.use(express.static(__dirname + "/public"));
+
+app.get('/customerList', function(request, response) {
+	console.log("I received a GET response");
+
+	customer1 = {
+		name: 'Abhijeet',
+		email: 'abhijeet@testemail.com',
+		phone: '1111111111'
+	};
+
+	customer2 = {
+		name: 'Piyush',
+		email: 'piyush@testemail.com',
+		phone: '2222222222'
+	};
+
+	customer3 = {
+		name: 'Ayush',
+		email: 'ayush@testemail.com',
+		phone: '3333333333'
+	};
+
+	var customerList = [customer1, customer2, customer3];
+	response.json(customerList);
+});
 
 app.listen(3000);
 console.log("Server running on port 3000");
