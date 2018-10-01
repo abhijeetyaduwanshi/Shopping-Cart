171113f376703bd89bd9731e359137e479f1be40
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2018-10-01 15:58:20 -0400

    Removed dummy data, hooked up mongodb to the application, created get and post requests to use data from database

--- public/controllers/customerController.js
+++ public/controllers/customerController.js
@@ -1,10 +1,23 @@
 var myApp = angular.module('myApp', []);
-
-myApp.controller('appCtrl', ['$scope', '$http',	function($scope, $http) {
+myApp.controller('appCtrl', ['$scope', '$http', function($scope, $http) {
 	console.log("Hello world from the controller");
 
-	$http.get('/customerList').then(function(response) {
-		console.log("I got the data I requested");
-		$scope.customerList = response.data;
-	});
+	var refresh = function() {
+		$http.get('/customerList').then(function(response) {
+			console.log("I got the data I requested");
+			$scope.customerList = response.data;
+		});
+	};
+	refresh();
+
+	$scope.addCustomer = function() {
+		console.log($scope.customer);
+		$http.post('/customerList', $scope.customer).then(function(response) {
+			console.log(response);
+			refresh();
+			if($scope.customer !== undefined) {
+				$scope.customer = "";
+			};
+		});
+	};
 }]);
