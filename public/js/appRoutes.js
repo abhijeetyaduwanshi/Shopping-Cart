118ebb5b1bac3fccd6d9be890054c6837b7f3622
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2019-02-18 22:34:14 -0500

    Updated the router for the home page

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,9 +1,8 @@
 var myApp = angular.module('shoppingCartApp', ['ngRoute', 'shoppingCartApp.myCustomNavBar']);
 myApp.config(function($routeProvider) {
 	console.log("Hello world from the route provider");
-	$routeProvider.when('/testOne', {
-		templateUrl: '../../views/testOne.html'
-	}).when('/testTwo', {
-		templateUrl: '../../views/testTwo.html'
+	$routeProvider
+	.when('/', {
+		templateUrl: '../views/home.html'
 	});
 });
