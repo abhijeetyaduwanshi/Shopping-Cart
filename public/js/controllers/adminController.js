00609fee84e09fc7ff7198258999ae22a44e5c0e
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2019-05-08 16:58:37 -0400

    admin page validate cat id, cat route, cat img path, cat title and cat desc

--- public/js/controllers/adminController.js
+++ public/js/controllers/adminController.js
@@ -16,13 +16,87 @@ controller.controller('adminCtrl', ['$scope', '$http', function($scope, $http) {
 	};
 	refresh();
 
+	// this function is to prepare the categoryId from categoryName
+	var categoryIdValueFunction = function(categoryName) {
+		var categoryIdValue = "category-";
+		if (categoryName) {
+			var categoryNameWords = categoryName.split(" ");
+			categoryIdValue = categoryIdValue.concat(categoryNameWords[0].toLowerCase());
+			for (var i = 1, len = categoryNameWords.length; i < len; i++) {
+				categoryIdValue = categoryIdValue.concat(categoryNameWords[i].charAt(0).toUpperCase() + categoryNameWords[i].slice(1));
+			}
+		}
+		return categoryIdValue;
+	};
+
+	// this function is to prepare the categoryRoute from categoryName
+	var categoryRouteValueFunction = function(categoryName) {
+		var categoryRouteValue = "#!/";
+		if (categoryName) {
+			var categoryNameWords = categoryName.split(" ");
+			for (var i = 0, len = categoryNameWords.length; i < len; i++) {
+				categoryRouteValue = categoryRouteValue.concat(categoryNameWords[i].charAt(0).toUpperCase() + categoryNameWords[i].slice(1));
+			}
+		}
+		return categoryRouteValue;
+	};
+
+	// this function is to prepare the categoryImgPath from categoryName
+	var categoryImgPathValueFunction = function(categoryName) {
+		var categoryImgPathValue = "images/categoryItems/";
+		if (categoryName) {
+			var categoryNameWords = categoryName.split(" ");
+			for (var i = 0, len = categoryNameWords.length; i < len; i++) {
+				categoryImgPathValue = categoryImgPathValue.concat(categoryNameWords[i].charAt(0).toLowerCase() + categoryNameWords[i].slice(1));
+				if (i < len-1) {
+					categoryImgPathValue = categoryImgPathValue.concat("-");
+				}
+			}
+			categoryImgPathValue = categoryImgPathValue.concat(".png");
+		}
+		return categoryImgPathValue;
+	};
+
+	// this function is to properly formate the categoryName
+	var categoryTitleValueFunction = function(categoryName) {
+		var categoryTitleValue = "";
+		if (categoryName) {
+			var categoryNameWords = categoryName.split(" ");
+			for (var i = 0, len = categoryNameWords.length; i < len; i++) {
+				categoryTitleValue = categoryTitleValue.concat(categoryNameWords[i].charAt(0).toUpperCase() + categoryNameWords[i].slice(1));
+				if (i < len-1) {
+					categoryTitleValue = categoryTitleValue.concat(" ");
+				}
+			}
+		}
+		return categoryTitleValue;
+	};
+
+	// this function is to properly formate the categoryDescription
+	var categoryDescriptionValueFunction = function(categoryDescription) {
+		var categoryDescriptionValue = "";
+		if (categoryDescription) {
+			categoryDescriptionValue = categoryDescription.charAt(0).toUpperCase() + categoryDescription.slice(1);
+			if (categoryDescription[categoryDescription.length -1] != ".") {
+				categoryDescriptionValue = categoryDescriptionValue.concat(".");
+			}
+		}
+		return categoryDescriptionValue;
+	};
+
 	// this method is to post the category data
 	$scope.addCategoryItem = function() {
-		// console.log($scope.categoryItem);
+		var catItem = $scope.categoryItem;
+		catItem.categoryId = categoryIdValueFunction(catItem.categoryTitle);
+		catItem.categoryRoute = categoryRouteValueFunction(catItem.categoryTitle);
+		catItem.categoryImage = categoryImgPathValueFunction(catItem.categoryTitle);
+		catItem.categoryTitle = categoryTitleValueFunction(catItem.categoryTitle);
+		catItem.categoryDescription = categoryDescriptionValueFunction(catItem.categoryDescription);
+		// console.log(catItem);
 		$http({
 			method: 'POST',
 			url: '/Admin',
-			data: $scope.categoryItem
+			data: catItem
 		}).then(function success(response) {
 			// console.log(response);
 			refresh();
