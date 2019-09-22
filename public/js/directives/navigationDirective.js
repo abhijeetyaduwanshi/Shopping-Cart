4ca9d1a2011d82273a8244ad5e804bced3f68a1f
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2019-09-22 19:53:40 -0400

    navigation directive removed unnecessary code

--- public/js/directives/navigationDirective.js
+++ public/js/directives/navigationDirective.js
@@ -1,7 +1,5 @@
 var myApp = angular.module('shoppingCartApp.myCustomNavBar', []);
-myApp.controller('navigationDirectiveCtrl', ['$scope', function($scope) {
-    // console.log("Hello world from the navigation directive controller");
-}]).directive('myNavigation', function() {
+myApp.directive('myNavigation', function() {
     return {
         templateUrl: '../../views/navigationDirective.html'
     };
