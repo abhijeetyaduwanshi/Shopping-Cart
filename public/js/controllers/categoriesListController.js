10a167e17516de815dbee6b22ee177238a3f9dfc
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-01-16 11:48:17 -0500

    updated home page js to es6 and updated jsdocs

--- public/js/controllers/categoriesListController.js
+++ public/js/controllers/categoriesListController.js
@@ -1,13 +1,13 @@
-var controller = angular.module('shoppingCartApp.categoriesListController', []);
+controller = angular.module('shoppingCartApp.categoriesListController', []);
 controller.controller('categoriesListCtrl',['$scope', '$http', function($scope, $http) {
-    // console.log("Hello world from the categories list controller");
 
-    // this method is to get the category list data
+    /**
+     * get category list data
+     */
     $http({
         method: 'GET',
         url: '/Home'
     }).then(function success(response) {
-        // console.log("I got the data I requested");
         $scope.categoryItemsList = response.data;
     }, function errorCallback(error) {
         // TODO: add error page code here
