590556a8538f4b1062f1d15d4d164bd7570710bd
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-01-16 16:12:43 -0500

    updated admin category page js to es6 and updated jsdocs

--- public/js/controllers/adminController.js
+++ public/js/controllers/adminController.js
@@ -1,14 +1,14 @@
-var controller = angular.module('shoppingCartApp.adminController', []);
+controller = angular.module('shoppingCartApp.adminController', []);
 controller.controller('adminCtrl', ['$scope', '$http', function($scope, $http) {
-    // console.log("Hello world from the admin controller");
 
-    // this method is to get the category data
-    var refresh = function() {
+    /**
+     * get category data
+     */
+    const refresh = () => {
         $http({
             method: 'GET',
             url: '/Admin'
         }).then(function success(response) {
-            // console.log("I got the data I requested");
             $scope.categoryItemsList = response.data;
         }, function errorCallback(error) {
             // TODO: add error page code here
@@ -16,37 +16,49 @@ controller.controller('adminCtrl', ['$scope', '$http', function($scope, $http) {
     };
     refresh();
 
-    // this function is to prepare the categoryId from categoryName
-    var categoryIdValueFunction = function(categoryName) {
-        var categoryIdValue = "category-";
+    /**
+     * prepare categoryId from categoryName
+     * 
+     * @param  {} categoryName, of the category
+     */
+    const categoryIdValueFunction = categoryName => {
+        let categoryIdValue = "category-";
         if (categoryName) {
-            var categoryNameWords = categoryName.split(" ");
+            const categoryNameWords = categoryName.split(" ");
             categoryIdValue = categoryIdValue.concat(categoryNameWords[0].toLowerCase());
-            for (var i = 1, len = categoryNameWords.length; i < len; i++) {
+            for (let i = 1, len = categoryNameWords.length; i < len; i++) {
                 categoryIdValue = categoryIdValue.concat(categoryNameWords[i].charAt(0).toUpperCase() + categoryNameWords[i].slice(1));
             }
         }
         return categoryIdValue;
     };
 
-    // this function is to prepare the categoryRoute from categoryName
-    var categoryRouteValueFunction = function(categoryName) {
-        var categoryRouteValue = "#!/category/";
+    /**
+     * prepare categoryRoute from categoryName
+     * 
+     * @param  {} categoryName, of the category
+     */
+    const categoryRouteValueFunction = categoryName => {
+        let categoryRouteValue = "#!/category/";
         if (categoryName) {
-            var categoryNameWords = categoryName.split(" ");
-            for (var i = 0, len = categoryNameWords.length; i < len; i++) {
+            const categoryNameWords = categoryName.split(" ");
+            for (let i = 0, len = categoryNameWords.length; i < len; i++) {
                 categoryRouteValue = categoryRouteValue.concat(categoryNameWords[i].charAt(0).toUpperCase() + categoryNameWords[i].slice(1));
             }
         }
         return categoryRouteValue;
     };
 
-    // this function is to prepare the categoryImgPath from categoryName
-    var categoryImgPathValueFunction = function(categoryName) {
-        var categoryImgPathValue = "images/categoryItems/";
+    /**
+     * prepare categoryImgPath from categoryName
+     * 
+     * @param  {} categoryName, of the category
+     */
+    const categoryImgPathValueFunction = categoryName => {
+        let categoryImgPathValue = "images/categoryItems/";
         if (categoryName) {
-            var categoryNameWords = categoryName.split(" ");
-            for (var i = 0, len = categoryNameWords.length; i < len; i++) {
+            const categoryNameWords = categoryName.split(" ");
+            for (let i = 0, len = categoryNameWords.length; i < len; i++) {
                 categoryImgPathValue = categoryImgPathValue.concat(categoryNameWords[i].charAt(0).toLowerCase() + categoryNameWords[i].slice(1));
                 if (i < len-1) {
                     categoryImgPathValue = categoryImgPathValue.concat("-");
@@ -57,12 +69,16 @@ controller.controller('adminCtrl', ['$scope', '$http', function($scope, $http) {
         return categoryImgPathValue;
     };
 
-    // this function is to properly formate the categoryName
-    var categoryTitleValueFunction = function(categoryName) {
-        var categoryTitleValue = "";
+    /**
+     * prepare categoryTitle from categoryName
+     * 
+     * @param  {} categoryName, of the category
+     */
+    const categoryTitleValueFunction = categoryName => {
+        let categoryTitleValue = "";
         if (categoryName) {
             var categoryNameWords = categoryName.split(" ");
-            for (var i = 0, len = categoryNameWords.length; i < len; i++) {
+            for (let i = 0, len = categoryNameWords.length; i < len; i++) {
                 categoryTitleValue = categoryTitleValue.concat(categoryNameWords[i].charAt(0).toUpperCase() + categoryNameWords[i].slice(1));
                 if (i < len-1) {
                     categoryTitleValue = categoryTitleValue.concat(" ");
@@ -72,9 +88,13 @@ controller.controller('adminCtrl', ['$scope', '$http', function($scope, $http) {
         return categoryTitleValue;
     };
 
-    // this function is to properly formate the categoryDescription
-    var categoryDescriptionValueFunction = function(categoryDescription) {
-        var categoryDescriptionValue = "";
+    /**
+     * prepare categoryDescription from categoryName
+     * 
+     * @param  {} categoryDescription, of the category
+     */
+    const categoryDescriptionValueFunction = categoryDescription => {
+        let categoryDescriptionValue = "";
         if (categoryDescription) {
             categoryDescriptionValue = categoryDescription.charAt(0).toUpperCase() + categoryDescription.slice(1);
             if (categoryDescription[categoryDescription.length -1] != ".") {
@@ -84,21 +104,22 @@ controller.controller('adminCtrl', ['$scope', '$http', function($scope, $http) {
         return categoryDescriptionValue;
     };
 
-    // this method is to post the category data
-    $scope.addCategoryItem = function() {
-        var catItem = $scope.categoryItem;
+    /**
+     * send category data
+     */
+    $scope.addCategoryItem = () => {
+        const catItem = $scope.categoryItem;
         catItem.categoryId = categoryIdValueFunction(catItem.categoryTitle);
         catItem.categoryRoute = categoryRouteValueFunction(catItem.categoryTitle);
         catItem.categoryImage = categoryImgPathValueFunction(catItem.categoryTitle);
         catItem.categoryTitle = categoryTitleValueFunction(catItem.categoryTitle);
         catItem.categoryDescription = categoryDescriptionValueFunction(catItem.categoryDescription);
-        // console.log(catItem);
+
         $http({
             method: 'POST',
             url: '/Admin',
             data: catItem
         }).then(function success(response) {
-            // console.log(response);
             refresh();
             $scope.categoryItem = {};
         }, function errorCallback(error) {
@@ -106,15 +127,16 @@ controller.controller('adminCtrl', ['$scope', '$http', function($scope, $http) {
         });
     };
 
-    // this function is to view the details of a category item
-    // params: id, of the category
-    $scope.viewCategoryDetails = function(id) {
-        // console.log(id);
+    /**
+     * view category details
+     * 
+     * @param  {} id, of the category
+     */
+    $scope.viewCategoryDetails = id => {
         $http({
             method: 'GET',
-            url: '/Admin/' + id
+            url: `/Admin/${id}`
         }).then(function success(response) {
-            // console.log(response);
             $scope.selectedCategoryItem = {};
             $scope.selectedCategoryItem = response.data[0];
         }, function errorCallback(error) {
@@ -122,38 +144,40 @@ controller.controller('adminCtrl', ['$scope', '$http', function($scope, $http) {
         });
     };
 
-    // this function is to delete category item
-    // params: id, of the category
-    $scope.removeCat = function(id) {
-        // console.log("delete " + id);
+    /**
+     * delete category item
+     * 
+     * @param  {} id, of the category
+     */
+    $scope.removeCat = id => {
         $http({
             method: 'DELETE',
-            url: '/Admin/' + id
+            url: `/Admin/${id}`
         }).then(function success(response) {
-            // console.log(response);
             refresh();
         }, function errorCallback(error) {
             // TODO: add error page code here
         });
     };
 
-    // this method is to put the category data for a particular category
-    // params: id, of the category
-    // TODO: edit should alter the categories table and also alter the table itself
-    $scope.editCategoryItem = function(id) {
-        var catItem = $scope.selectedCategoryItem;
+    /**
+     * edit category data
+     * 
+     * @param  {} id, of the category
+     */
+    $scope.editCategoryItem = id => {
+        const catItem = $scope.selectedCategoryItem;
         catItem.categoryId = categoryIdValueFunction(catItem.categoryTitle);
         catItem.categoryRoute = categoryRouteValueFunction(catItem.categoryTitle);
         catItem.categoryImage = categoryImgPathValueFunction(catItem.categoryTitle);
         catItem.categoryTitle = categoryTitleValueFunction(catItem.categoryTitle);
         catItem.categoryDescription = categoryDescriptionValueFunction(catItem.categoryDescription);
-        // console.log(catItem);
+
         $http({
             method: 'PUT',
-            url: '/AdminEdit/' + id,
+            url: `/AdminEdit/${id}`,
             data: catItem
         }).then(function success(response) {
-            // console.log(response);
             $scope.selectedCategoryItem = {};
             refresh();
         }, function errorCallback(error) {
@@ -161,9 +185,13 @@ controller.controller('adminCtrl', ['$scope', '$http', function($scope, $http) {
         });
     };
 
-    // this is for the tab view for the admin category options
+    /**
+     * tab view for admin category options
+     * 
+     * @param  {} tabName, of the category
+     */
     $scope.isActive = "editCategory";
-    $scope.activeButton = function(tabName) {
+    $scope.activeButton = tabName => {
         $scope.isActive = tabName;
     };
 }]);
