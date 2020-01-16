0fcdac7a68f1701212a84b96012a455deff238e1
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-01-16 13:49:16 -0500

    updated cart page js to es6 and updated jsdocs

--- public/js/controllers/navigationController.js
+++ public/js/controllers/navigationController.js
@@ -1,24 +1,26 @@
-var controller = angular.module('shoppingCartApp.navigationController', []);
+controller = angular.module('shoppingCartApp.navigationController', []);
 controller.controller('navigationCtrl',['$scope', '$http', function($scope, $http) {
-    // console.log("Hello world from the navigation controller");
 
-    // this method is to get the navigation data
+    /**
+     * get navigation data
+     */
     $http({
         method: 'GET',
         url: '/Navigation'
     }).then(function success(response) {
-        // console.log("I got the data I requested");
         $scope.navigationItems = response.data;
     }, function errorCallback(error) {
         // TODO: add error page code here
     });
 
-    // this method is to get the carted item count from local storage
-    var getCartCount = function() {
+    /**
+     * get carted item count
+     */
+    const getCartCount = () => {
         $scope.itemsCount = 0;
-        var cartedItems = JSON.parse(localStorage.getItem("cartedItems"));
+        const cartedItems = JSON.parse(localStorage.getItem("cartedItems"));
         if (cartedItems !== null && cartedItems.length !== 0) {
-            for (var i = 0; i < cartedItems.length; i++) {
+            for (let i = 0; i < cartedItems.length; i++) {
                 $scope.itemsCount += cartedItems[i].count++;
             }
         }
