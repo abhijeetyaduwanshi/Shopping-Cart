711767ce1eef4a985ce71b746533c143e242712e
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-02-02 14:22:06 -0500

    FE: tests for getcat and getnavigation functions

--- src/app/services/catService/cat.service.spec.ts
+++ src/app/services/catService/cat.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { CatService } from './cat.service';
@@ -17,5 +17,27 @@ describe('CatService', () => {
     it('should have getCats function', () => {
         const service: CatService = TestBed.get(CatService);
         expect(service.getCats).toBeTruthy();
+    });
+
+    it('getCats should return cat list', () => {
+        const service: CatService = TestBed.get(CatService);
+        const http = TestBed.get(HttpTestingController);
+
+        const expectedCat = [{
+            "_id": "abcd12345",
+            "categoryTitle": "Some name",
+            "categoryDescription": "Some Description.",
+            "categoryId": "some-id",
+            "categoryRoute": "some/route",
+            "categoryImage": "images/some/image.png"
+        }];
+        let actualCat: any = [];
+
+        service.getCats().subscribe(data => {
+            actualCat = data;
+        });
+
+        http.expectOne('http://localhost:8080/api').flush(expectedCat);
+        expect(actualCat).toEqual(expectedCat);
     });
 });
--- src/app/services/navigationService/navigation.service.spec.ts
+++ src/app/services/navigationService/navigation.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { NavigationService } from './navigation.service';
@@ -17,5 +17,24 @@ describe('NavigationService', () => {
     it('should have getNavigation function', () => {
         const service: NavigationService = TestBed.get(NavigationService);
         expect(service.getNavigation).toBeTruthy();
+    });
+
+    it('getNavigation should return navigation list', () => {
+        const service: NavigationService = TestBed.get(NavigationService);
+        const http = TestBed.get(HttpTestingController);
+
+        const expectedNavigation = [{
+            "_id": "abcd12345",
+            "categoryTitle": "Some name",
+            "categoryRoute": "some/route"
+        }];
+        let actualNavigation: any = [];
+
+        service.getNavigation().subscribe(data => {
+            actualNavigation = data;
+        });
+
+        http.expectOne('http://localhost:8080/api/navigation').flush(expectedNavigation);
+        expect(actualNavigation).toEqual(expectedNavigation);
     });
 });
