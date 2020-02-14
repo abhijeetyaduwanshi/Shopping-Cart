acc5bc61d567a4beb34e9e3559ffd0e78401fd75
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-02-13 20:41:49 -0500

    FE: created products component and routing for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CatComponent } from './components/cat/cat.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { ProductComponent } from './components/product/product.component';
 import { CatService } from './services/catService/cat.service';
 import { NavigationService } from './services/navigationService/navigation.service';
 import { AngularMaterialModule } from './material.module';
@@ -14,19 +16,26 @@ import { AngularMaterialModule } from './material.module';
     declarations: [
         AppComponent,
         CatComponent,
-        NavigationComponent
+        NavigationComponent,
+        ProductComponent
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
         BrowserAnimationsModule,
-        AngularMaterialModule
+        AngularMaterialModule,
+        RouterModule.forRoot([
+            { path: '', component: CatComponent },
+            { path: 'cat/:categoryId', component: ProductComponent }
+        ])
     ],
     providers: [
         CatService,
         NavigationService
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [
+        AppComponent
+    ]
 })
 
 export class AppModule { }
