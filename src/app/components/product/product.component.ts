4caed57e0e15aad388e39c7d9d6406d3cbcc1293
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-05-27 14:35:04 -0400

    FE: clicking add to cart button adds product to local storage

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -20,10 +20,44 @@ export class ProductComponent implements OnInit {
     }
 
     private getProduct = () => {
-        let catName = this.activeRoute.snapshot.paramMap.get('categoryId').substr(this.activeRoute.snapshot.paramMap.get('categoryId').lastIndexOf('-') + 1);
+        const catName = this.activeRoute.snapshot.paramMap.get('categoryId').substr(this.activeRoute.snapshot.paramMap.get('categoryId').lastIndexOf('-') + 1);
 
         this.productApi.getProduct(catName).subscribe(data => {
             this.productData = data;
         })
+    }
+
+    private addToCart = (productId) => {
+        alert("Item added to cart");
+        const catName = this.activeRoute.snapshot.paramMap.get('categoryId').substr(this.activeRoute.snapshot.paramMap.get('categoryId').lastIndexOf('-') + 1);
+        const products = [];
+        let returningProduct = false;
+
+        const productDetails = {
+            productId: productId,
+            type: catName,
+            count: 1
+        };
+
+        products.push(productDetails);
+
+        if (JSON.parse(localStorage.getItem("cartedProducts")) == null) {
+            localStorage.setItem("cartedProducts", JSON.stringify(products));
+        } else {
+            const cartedProducts = JSON.parse(localStorage.getItem("cartedProducts"));
+            for (let i = 0; i < cartedProducts.length; i++) {
+                if (cartedProducts[i].productId == productDetails.productId) {
+                    cartedProducts[i].count++;
+                    returningProduct = true;
+                    break;
+                }
+            }
+
+            if (!returningProduct) {
+                cartedProducts.push(productDetails);
+            }
+
+            localStorage.setItem("cartedProducts", JSON.stringify(cartedProducts));
+        }
     }
 }
