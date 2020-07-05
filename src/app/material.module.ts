13e8aff44249a5c03e4fcfc01a6dd630a70747c5
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-07-05 17:03:07 -0400

    FE: Home page - sign in or create a new account options

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,13 +3,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
     exports: [
         MatButtonModule,
         MatCardModule,
         MatDialogModule,
-        MatMenuModule
+        MatMenuModule,
+        MatTabsModule
     ]
 })
 
