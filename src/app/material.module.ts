e5290836323c6b7cb7cb174591be1debf68756cf
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-07-06 18:53:24 -0400

    Material Module: arranged dependencies alphabetically

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTabsModule } from '@angular/material/tabs';
+import { NgModule } from '@angular/core';
 
 @NgModule({
     exports: [
