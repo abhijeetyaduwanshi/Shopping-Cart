0b60a16218e92051d6dbd9c67c818e0dbeaf0b84
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-07-10 00:54:44 -0400

    FE: Home page - designed and written code for create account form on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,7 +14,6 @@ export class HomeComponent implements OnInit {
     ngOnInit(): void { }
 
     private signInEmail = new FormControl('', [Validators.required, Validators.email]);
-
     private getSignInEmailErrorMessage = () => {
         if (this.signInEmail.hasError('required')) {
             return 'You must enter a value';
@@ -24,9 +23,54 @@ export class HomeComponent implements OnInit {
 
     private signInPassword = new FormControl('', [Validators.required]);
     private hideSignInPassword = true;
-
     private getSignInPasswordErrorMessage = () => {
         if (this.signInPassword.hasError('required')) {
+            return 'You must enter a value';
+        }
+    }
+
+    private createAccountFirstName = new FormControl('', [Validators.required]);
+    private getCreateAccountFirstNameErrorMessage = () => {
+        if (this.createAccountFirstName.hasError('required')) {
+            return 'You must enter a value';
+        }
+    }
+
+    private createAccountLastName = new FormControl('', [Validators.required]);
+    private getCreateAccountLastNameErrorMessage = () => {
+        if (this.createAccountLastName.hasError('required')) {
+            return 'You must enter a value';
+        }
+    }
+
+    private createAccountEmail = new FormControl('', [Validators.required, Validators.email]);
+    private getCreateAccountEmailErrorMessage = () => {
+        if (this.createAccountEmail.hasError('required')) {
+            return 'You must enter a value';
+        }
+        return this.createAccountEmail.hasError('email') ? 'Not a valid email' : '';
+    }
+
+    private createAccountConfirmEmail = new FormControl('', [Validators.required, Validators.email]);
+    private getCreateAccountConfirmEmailErrorMessage = () => {
+        if (this.createAccountConfirmEmail.hasError('required')) {
+            return 'You must enter a value';
+        }
+        return this.createAccountConfirmEmail.hasError('email') ? 'Not a valid email' : '';
+    }
+
+    private createAccountPassword = new FormControl('', [Validators.required]);
+    private hideCreateAccountPassword = true;
+    private getCreateAccountPasswordErrorMessage = () => {
+        if (this.createAccountPassword.hasError('required')) {
+            return 'You must enter a value';
+        }
+    }
+
+    private createAccountconfirmPassword = new FormControl('', [Validators.required]);
+    private hideCreateAccountConfirmPassword = true;
+    private getCreateAccountConfirmPasswordErrorMessage = () => {
+        if (this.createAccountconfirmPassword.hasError('required')) {
             return 'You must enter a value';
         }
     }
