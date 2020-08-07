adf82f48fa577dd6c4a810cfda275c0f088df6dc
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-08-06 20:03:03 -0400

    sign in to your profile component - fixed failing test

--- src/app/components/home/homeComponents/sign-in-to-your-profile/sign-in-to-your-profile.component.spec.ts
+++ src/app/components/home/homeComponents/sign-in-to-your-profile/sign-in-to-your-profile.component.spec.ts
@@ -1,25 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { AppModule } from './../../../../app.module';
 import { SignInToYourProfileComponent } from './sign-in-to-your-profile.component';
 
 describe('SignInToYourProfileComponent', () => {
-  let component: SignInToYourProfileComponent;
-  let fixture: ComponentFixture<SignInToYourProfileComponent>;
+    let component: SignInToYourProfileComponent;
+    let fixture: ComponentFixture<SignInToYourProfileComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SignInToYourProfileComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [AppModule],
+            declarations: [ SignInToYourProfileComponent ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SignInToYourProfileComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SignInToYourProfileComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
