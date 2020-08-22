3dc97c2b53f74f70d249e98a73b21738ad4c14f2
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-08-22 15:35:41 -0400

    Cat and Navigation service - updated error handling

--- src/app/services/catService/cat.service.ts
+++ src/app/services/catService/cat.service.ts
@@ -1,5 +1,9 @@
+import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Cat } from './cat';
 
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -18,6 +22,27 @@ export class CatService {
      * GET cats from the server
      */
     getCats() {
-        return this.http.get(`${this.endpoint}`);
+        return this.http.get(`${this.endpoint}`)
+        .pipe(
+            catchError(this.handleError('getCats'))
+        ) as Observable<Cat[]>;
+    }
+
+    /**
+     * Returns a function that handles Http operation failures
+     * This error handler lets the app continue to run as if no error occurred
+     * 
+     * @param  {operation='operation'} name of the operation that failed
+     */
+    private handleError<T>(operation = 'operation') {
+        return (error: HttpErrorResponse): Observable<T> => {
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
+
+            const message = (error.error instanceof ErrorEvent) ? error.error.message : `server returned code ${error.status} with body "${error.error}"`;
+
+            // TODO: better job of transforming error for user consumption
+            throw new Error(`${operation} failed: ${message}`);
+        };
     }
 }
