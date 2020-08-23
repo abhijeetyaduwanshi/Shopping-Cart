24a9ba22739821d79b7dc25df23c0315998d8068
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-08-22 21:31:52 -0400

    Unit test: Product service - updated unit test to use spies

--- src/app/services/productService/product.service.spec.ts
+++ src/app/services/productService/product.service.spec.ts
@@ -1,75 +1,45 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
-
+import { asyncData } from 'src/async-observable-helpers';
+import { getTestProducts } from './testing/test-products';
 import { ProductService } from './product.service';
 
-describe('ProductService', () => {
+describe('ProductService (with spies)', () => {
+    let httpClientSpy: { get: jasmine.Spy };
     let service: ProductService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule],
-            providers: [ProductService]
-        });
-        service = TestBed.get(ProductService);
+        httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+        service = new ProductService(httpClientSpy as any);
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
-    });
-
-    it('should have getProducts function', () => {
+    it('should return expected products (HttpClient called once)', () => {
+        const expectedProduct = getTestProducts();
         const catName = "someCat";
-        expect(service.getProducts(catName)).toBeTruthy();
+
+        httpClientSpy.get.and.returnValue(asyncData(expectedProduct));
+
+        service.getProducts(catName).subscribe(
+            products => expect(products).toEqual(expectedProduct, 'expected products'),
+            fail
+        );
+        expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
     });
 
-    it('getProducts should return product list', () => {
-        const http = TestBed.get(HttpTestingController);
-
-        const catName = "someCat";
-        const expectedProduct = [{
-            "_id": "abcd12345",
-            "productTitle": "Some name",
-            "productDescription": "Some Description.",
-            "productIsSpicy": "mild",
-            "productIsVegetarian": "true",
-            "productPrice": "1.99",
-            "productImage": "images/some/image.png"
-        }];
-        let actualProduct: any = [];
-
-        service.getProducts(catName).subscribe(data => {
-            actualProduct = data;
-        });
-
-        http.expectOne(`http://localhost:8080/api/get-products/${catName}`).flush(expectedProduct);
-        expect(actualProduct).toEqual(expectedProduct);
-    });
-
-    it('should have getProduct function', () => {
-        const id = "productId";
-        const catName = "someCat";
-        expect(service.getProduct(id, catName)).toBeTruthy();
-    });
-
-    it('getProduct should return product id, title, price and image', () => {
-        const http = TestBed.get(HttpTestingController);
-
-        const id = "productId";
-        const catName = "someCat";
+    it('should return expected product\'s product id, title, price and image', () => {
         const expectedProduct = [{
             "_id": "abcd12345",
             "productTitle": "Some name",
             "productPrice": "1.99",
             "productImage": "images/some/image.png"
         }];
-        let actualProduct: any = [];
+        const id = "abcd12345";
+        const catName = "someCat";
 
-        service.getProduct(id, catName).subscribe(data => {
-            actualProduct = data;
-        });
+        httpClientSpy.get.and.returnValue(asyncData(expectedProduct));
 
-        http.expectOne(`http://localhost:8080/api/get-product/${id}/${catName}`).flush(expectedProduct);
-        expect(actualProduct).toEqual(expectedProduct);
+        service.getProduct(id, catName).subscribe(
+            product => expect(product).toEqual(expectedProduct, 'expected product'),
+            fail
+        );
+        expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
     });
 });
