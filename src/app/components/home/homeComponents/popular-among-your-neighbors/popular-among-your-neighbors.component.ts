6e2a0e19729e56785bf51dac52a472e5204c5385
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-08-25 22:16:06 -0400

    FE: Home page - Popular among your neighbors = added dummy products with there images and name displayed

--- src/app/components/home/homeComponents/popular-among-your-neighbors/popular-among-your-neighbors.component.ts
+++ src/app/components/home/homeComponents/popular-among-your-neighbors/popular-among-your-neighbors.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from './../../../../services/productService/product.service';
+
 @Component({
     selector: 'app-popular-among-your-neighbors',
     templateUrl: './popular-among-your-neighbors.component.html',
@@ -11,9 +13,32 @@ import { Component, OnInit } from '@angular/core';
  */
 export class PopularAmongYourNeighborsComponent implements OnInit {
 
+    private popularData: any = [];
+    private popularItems: any = [
+        {"productId":"5eff7131dcc5c84b47253f6a","type":"pizzas"},
+        {"productId":"5efe6c1fdcc5c84b47253f3d","type":"chicken"},
+        {"productId":"5efe6dc9dcc5c84b47253f47","type":"desserts"},
+        {"productId":"5eff72cadcc5c84b47253f76","type":"sandwiches"},
+        {"productId":"5eff7264dcc5c84b47253f73","type":"salads"}
+    ]
+
     // popular among your neighbors constructor
-    constructor() { }
+    constructor(private productApi: ProductService) { }
 
     // popular among your neighbors ngOnInit
-    ngOnInit(): void { }
+    ngOnInit(): void {
+        this.populatePopularItems();
+    }
+
+    private populatePopularItems = () => {
+        for (let i = 0; i < this.popularItems.length; i++) {
+            this.getPopularItems(this.popularItems[i]);
+        }
+    }
+
+    private getPopularItems = (popularItem) => {
+        this.productApi.getProduct(popularItem.productId, popularItem.type).subscribe(data => {
+            this.popularData.push(data);
+        })
+    }
 }
