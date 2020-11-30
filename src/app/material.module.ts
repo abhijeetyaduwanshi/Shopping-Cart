5ee790bae31e532e0ab106d3d13d9b1ea57b0a77
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-11-30 14:49:38 -0500

    created first category item

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,10 @@
+import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-    exports: []
+    exports: [
+        MatCardModule
+    ]
 })
 
 export class AngularMaterialModule { }
