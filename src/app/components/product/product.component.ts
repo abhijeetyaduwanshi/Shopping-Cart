8c55752b9e4a9b469e16600d2e5f4897aada3cbb
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2020-12-02 15:29:45 -0500

    getting products from product service to product component

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,15 +1,34 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
+import { ProductService } from './../../services/productService/product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
 
-  constructor() { }
+export class ProductComponent implements OnInit {
+  productData: any = [];
+
+  constructor(private activatedRoute: ActivatedRoute, private productApi: ProductService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(params => {
+      const categoryTitle = params.get("categoryTitle");
+      this.getProducts(categoryTitle);
+    })
   }
 
+  /**
+   * GET all products
+   * 
+   * @param  {any} categoryTitle
+   */
+  getProducts = (categoryTitle: any) => {
+    this.productApi.getProducts(categoryTitle).subscribe(data => {
+      this.productData = data;
+    })
+  }
 }
--- src/app/services/productService/product.service.ts
+++ src/app/services/productService/product.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductService {
 
-  constructor() { }
+  corsAnywhere: string = 'https://cors-anywhere.herokuapp.com/';
+  endpoint: string = 'http://abhijeet-shopping-cart-app-be.herokuapp.com/api/products/';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * GET all products
+   * 
+   * @param  {string} categoryTitle
+   */
+  getProducts = (categoryTitle: string) => {
+    return this.http.get(`${this.corsAnywhere + this.endpoint + "/" + categoryTitle}`);
+  }
 }
