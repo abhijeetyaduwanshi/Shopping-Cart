65318eb3f61ec5aa1950dcd2b962ec9d057572f2
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2021-01-05 18:03:09 -0500

    using navigation service to populate the top navigation component

--- src/app/components/navigation/top-navigation/top-navigation.component.ts
+++ src/app/components/navigation/top-navigation/top-navigation.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NavigationService } from './../../../services/navigationService/navigation.service';
+
 @Component({
   selector: 'app-top-navigation',
   templateUrl: './top-navigation.component.html',
   styleUrls: ['./top-navigation.component.css']
 })
-export class TopNavigationComponent implements OnInit {
 
-  constructor() { }
+export class TopNavigationComponent implements OnInit {
+  navigationData: any = [];
+
+  constructor(private navigationApi: NavigationService) { }
 
   ngOnInit(): void {
+    this.getNavigation();
   }
 
+  /**
+   * GET navigation
+   */
+  getNavigation = () => {
+    this.navigationApi.getNavigation().subscribe(data => {
+      this.navigationData = data;
+    })
+  }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,13 @@
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 
 @NgModule({
     exports: [
         MatButtonModule,
-        MatCardModule
+        MatCardModule,
+        MatToolbarModule
     ]
 })
 
