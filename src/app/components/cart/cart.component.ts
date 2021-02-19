024d11559e1f4418d6fe2ea715207e5a20ea9044
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2021-02-19 00:15:04 -0500

    Added code for populating cart and created UI for it

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from './../../services/productService/product.service';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -8,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 
 export class CartComponent implements OnInit {
   cartedProducts = JSON.parse(localStorage.getItem("cartedProducts")) || [];
+  cartedProductData: any = [];
+  cartTotal: number = 0;
 
-  constructor() { }
+  constructor(private productApi: ProductService) { }
 
   ngOnInit(): void {
+    this.populateCart();
+  }
+
+  /**
+   * Populate cart
+   */
+  populateCart = () => {
+    if (this.cartedProducts.length > 0) {
+      for (let i = 0; i < this.cartedProducts.length; i++) {
+        let cartedProduct = this.cartedProducts[i];
+        this.productApi.getProduct(cartedProduct.type, cartedProduct.productId).subscribe(data => {
+          data["productQuantity"] = cartedProduct.count;
+          data["productCalculatedPrice"] = data["productPrice"] * cartedProduct.count;
+          this.cartedProductData.push(data);
+          this.cartTotal += parseFloat(data["productCalculatedPrice"]);
+        })
+      }
+    }
   }
 }
