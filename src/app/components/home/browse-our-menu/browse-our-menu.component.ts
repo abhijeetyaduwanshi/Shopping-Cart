02d486273afe3b2810e853ed2e5d5bf02192ed04
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2021-07-25 02:37:41 -0400

    Added code for browse our menu on home page (#11)

---
+++ src/app/components/home/browse-our-menu/browse-our-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-our-menu',
+  templateUrl: './browse-our-menu.component.html',
+  styleUrls: ['./browse-our-menu.component.css']
+})
+export class BrowseOurMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
