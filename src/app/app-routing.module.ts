042dde8db17d2d8f63792788e36c57788edf03bb
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2021-07-25 01:52:07 -0400

    Added code for popular items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { CartComponent } from './components/cart/cart.component';
 import { CategoryComponent } from './components/category/category.component';
+import { HomeComponent } from './components/home/home.component';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
-  { path: '', component: CategoryComponent },
+  { path: '', component: HomeComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'category', component: CategoryComponent },
   { path: 'category/:categoryTitle', component: ProductComponent }
 ];
 
