7e7926589b41b7321c1bf0af2a8d15eecfd537a5
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2021-07-25 17:12:07 -0400

    Added code for delivery or pickup options (#12)

---
+++ src/app/components/home/homeSubComponents/delivery-or-pickup/delivery-or-pickup.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-or-pickup',
+  templateUrl: './delivery-or-pickup.component.html',
+  styleUrls: ['./delivery-or-pickup.component.css']
+})
+export class DeliveryOrPickupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
